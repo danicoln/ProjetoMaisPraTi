084d8b3b7902b9d5bbd2809fa084a28b214c1015
danicoln <lincolndeveloper@outlook.com>
2022-10-12 20:56:04 -0300

    refatoração da classe UserService

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -1,5 +1,20 @@
 package service;
 
+import java.util.Scanner;
+
+import model.User;
+import repository.Repository;
+
 public class UserService {
 
+	/*Dependency Injections*/
+	Scanner sc;
+	Repository<User> repository = new Repository<>();
+	
+	
+	/*Constructor with parameters*/
+	
+	public UserService(Scanner sc) {
+		this.sc = sc;
+	}
 }
