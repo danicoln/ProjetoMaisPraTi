fee8fe66cef98ec46d36ba876e6cc8d9012e401d
danicoln <lincolndeveloper@outlook.com>
2022-10-12 20:56:49 -0300

    refatoração da classe UserRepository

--- src/repository/UserRepository.java
+++ src/repository/UserRepository.java
@@ -1,35 +1,11 @@
 package repository;
 
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.stream.Collectors;
+import model.User;
 
-import model.Person;
+public class UserRepository extends Repository<User> {
 
-public class TeatcherRepository {
-
-private Map<Integer, Person> database;
-	
-	public TeatcherRepository() {
-		this.database = new TreeMap<>();
+	public User findForCpf(String cpf) {
+		return findAll().stream().filter(u -> u.getCpf().equals(cpf)).findFirst().orElse(null);
 	}
 
-	/*CRUD*/
-	
-	public void create(Person teatcher) {
-		this.database.put(teatcher.getId(), teatcher);
-	}
-	
-	public List<Person> readAll(){
-		return this.database.values().stream().collect(Collectors.toList());
-	}
-	
-	public void update (Person teatcher, Integer id) { // verificar e implementar o metodo update
-		this.database.put(id, teatcher);
-	}
-	
-	public void deleteForId(Integer id) {
-		this.database.remove(id);
-	}
 }
