a87dbeba28a608a3ff5c6313fbcd280132d1e4a2
danicoln <lincolndeveloper@outlook.com>
2022-10-16 12:09:57 -0300

    Remoção de comentários e adição de novos métodos

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -1,22 +1,26 @@
 package service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Scanner;
 
+import exception.ProgramException;
+import menu.Menu;
 import model.Person;
 import model.Student;
 import model.User;
+import model.ValidaCpf;
 import repository.PersonRepository;
 import repository.Repository;
 import repository.StudentRepository;
+import repository.UserRepository;
 
 public class UserService {
 
 	/* Dependency Injections */
 	Scanner sc;
-	Repository<User> userRepository = new Repository<>();
-//	Repository<Person> personRepository = new Repository<>();
-//	Repository<Student> studentRepository = new Repository<>();
+	Repository<User> repository = new Repository<>();
+	UserRepository userRepository = new UserRepository();
 	PersonRepository personRepository = new PersonRepository();
 	StudentRepository studentRepository = new StudentRepository();
 
@@ -26,29 +30,23 @@ public class UserService {
 		this.sc = sc;
 	}
 
-	public User checkCpf(String cpf) {
-		List<Person> listPerson = this.personRepository.findAll();
-		for (Person person : listPerson) {
-			if (person.getCpf().equalsIgnoreCase(cpf)) {
+	public User checkCpf(String cpf) throws ProgramException {
+		List<User> listUser = this.userRepository.findAll();
+		for (User user : listUser) {
+			if (user.getCpf().equalsIgnoreCase(cpf)) {
+//				user.imprimeDados  IMPLEMENTAR
 				System.out.println("O CPF informado já existe para esta pessoa! Tente novamente!");
-				return person;
-			}
-		}
-
-		List<Student> listStudent = this.studentRepository.findAll();
-		for (Student student : listStudent) {
-			if (student.getCpf().equalsIgnoreCase(cpf)) {
-				System.out.println("O CPF informado já existe para este estudante! Tente novamente!");
-				return student;
+				return user;
 			}
 		}
 
 		return this.saveUser(cpf);
+
 	}
 
-	private User saveUser(String user) {
+	private User saveUser(String user) throws ProgramException {
 		System.out.println("Informe seu nome: ");
-		sc.next();
+		sc.nextLine();
 		String name = sc.nextLine();
 		System.out.println("Digite a data de nascimento: ");
 		String dateBirth = sc.nextLine();
@@ -56,50 +54,226 @@ public class UserService {
 		String phone = sc.nextLine();
 		System.out.println("Digite seu CPF: ");
 		String cpf = sc.nextLine();
+		if (ValidaCpf.isValidCPF(cpf) == false) {
+			try {
+				System.out.println("CPF inválido!!! Tente novamente!");
+			} catch (Exception e) {
+				throw new ProgramException("CPF inválido!" + e.getMessage());
+			}
+			return null;
+		}
 
-		System.out.println("Deseja informar nota final do curso:");
-		System.out.println("| 1. Informar");
-		System.out.println("| 2. Não informar");
+		System.out.println("Deseja informar nota final do curso:\n" + "| 1. Informar\n" + "| 2. Não informar");
 		int info = sc.nextInt();
 
 		if (info == 2) {
 			Person newPerson = new Person(name, dateBirth, phone, cpf);
-			this.personRepository.save(newPerson);
+
+//			newPerson.imprimeDados IMPLEMENTAR
 			System.out.println("Pessoa cadastrada com sucesso!");
-			return newPerson;
+			return this.personRepository.save(newPerson);
+
 		} else if (info == 1) {
 			System.out.println("Informe a nota final do curso: ");
+
 			double gradeFinal = sc.nextDouble();
 			Student student = new Student(name, gradeFinal, dateBirth, phone, cpf);
-			this.studentRepository.save(student);
-			return student;
+			System.out.println("Aluno(a) cadastrado(a) com sucesso!");
+//			sudent.imprimeDados IMPLEMENTAR
+			return this.studentRepository.save(student);
 		}
-		return null; // verificar o retorno
+		return null; // verificar o retorno - add exception
 	}
 
-//	public User findUser(String user) {
-//		int optionChoose = sc.nextInt();
-//		if (optionChoose == 1) {
-//			System.out.println("Informe o CPF da pessoa: ");
-//			String cpfPerson = sc.next();
-//
-//			Person person = this.personRepository.findForCpf(cpfPerson);
-//			person.printData();
-//			return person;
-//
-//		} else if (optionChoose == 2) {
-//			System.out.println("Informe o CPF do aluno: ");
-//			String cpfStudent = sc.next();
-//
-//			Student student = this.studentRepository.findForCpf(cpfStudent);
-//			student.printData();
-//			return student;
-//
-//		} else if (optionChoose == 3) {
-//			System.out.println("Operação encerrada!");
-//		}
-//		return null;
-//	}
-	
-	
+	public User fetchUser(String docCpf) {
+		int opChoose = sc.nextInt();
+
+		if (opChoose == 1) {
+			System.out.println("Informe o CPF da pessoa: ");
+			String cpfPerson = sc.next();
+
+			Person person = this.personRepository.findForCpf(docCpf);
+//			person.imprimeDados(); IMPLEMENTAR
+			return person;
+
+		} else if (opChoose == 2) {
+			System.out.println("Informe o CPF do aluno: ");
+			String cpfStudent = sc.next();
+
+			Student student = this.studentRepository.findForCpf(docCpf);
+//			student.imprimeDados(); IMPLEMENTAR
+			return student;
+
+		} else if (opChoose == 3) {
+			System.out.println("Encerrando");
+		}
+
+		return null;
+
+//		return usuario;
+
+	}
+
+	public User removeUser(String cpf) {
+		int opChoose = sc.nextInt();
+
+		if (opChoose == 2) {
+			System.out.println("Informe o CPF da pessoa: ");
+			String cpfPerson = sc.next();
+
+			Person person = this.personRepository.findForCpf(cpfPerson);
+
+			this.personRepository.removeForId(person.getId());
+
+			// ver se precisar retornar algo
+			System.out.println("Pessoa removida com sucesso!");
+
+		} else if (opChoose == 1) {
+			System.out.println("Informe o CPF do(a) aluno(a)");
+			String cpfStudent = sc.next();
+
+			Student student = this.studentRepository.findForCpf(cpfStudent);
+
+			this.studentRepository.removeForId(student.getId());
+
+			System.out.println("Pessoa removida com sucesso!");
+
+		} else if (opChoose == 3) {
+			System.out.println("Menu anterior");
+		}
+		return null;
+	}
+
+	public User update() {
+		int op = sc.nextInt();
+		if (op == 1) {
+			this.updateStudent();
+		} else if (op == 2) {
+			this.updatePerson();
+		}
+		return null;
+	}
+
+	public User updateStudent() {
+		System.out.println("Escolha as opções a serem atualizadas:\n" + "| 1. Atualizar nome\n"
+				+ "| 2. Atualizar data de nascimento\n" + "| 3. Atualizar telefone\n"
+				+ "| 4. Atualizar atualizar CPF\n");
+
+		int opStudent = sc.nextInt();
+		if (opStudent == 1) {
+			Menu.infoCpf();
+			String cpf = sc.next();
+			User student = this.studentRepository.findForCpf(cpf);
+			System.out.println("Informe o nome a ser atualizado: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			student.setName(name);
+			student.setDateUpdate(LocalDateTime.now());
+//			student.imprimeDados IMPLEMENTAR
+			System.out.println("| Nome atualizado com sucesso!");
+			return student;
+
+		} else if (opStudent == 2) {
+			Menu.infoCpf();
+			String cpf = sc.next();
+			User student = this.studentRepository.findForCpf(cpf);
+			System.out.println("Informe o nome a ser atualizado: ");
+			sc.nextLine();
+			String dtNasc = sc.nextLine();
+			student.setBirthDate(dtNasc);
+			student.setDateUpdate(LocalDateTime.now());
+//			student.imprimeDados IMPLEMENTAR
+			System.out.println("| Data de nascimento atualizada com sucesso!");
+			return student;
+
+		} else if (opStudent == 3) {
+			Menu.infoCpf();
+			String cpf = sc.next();
+			User student = this.studentRepository.findForCpf(cpf);
+			System.out.println("Informe o nome a ser atualizado: ");
+			sc.nextLine();
+			String phone = sc.nextLine();
+			student.setPhone(phone);
+			student.setDateUpdate(LocalDateTime.now());
+//			student.imprimeDados IMPLEMENTAR
+			System.out.println("| Telefone atualizado com sucesso!");
+			return student;
+
+		} else if (opStudent == 4) {
+			Menu.infoCpf();
+			String cpf = sc.next();
+			User student = this.studentRepository.findForCpf(cpf);
+			System.out.println("Informe o nome a ser atualizado: ");
+			sc.nextLine();
+			String cpfUpdate = sc.nextLine();
+			student.setCpf(cpfUpdate);
+			student.setDateUpdate(LocalDateTime.now());
+//			student.imprimeDados IMPLEMENTAR
+			System.out.println("| CPF atualizado com sucesso!");
+			return student;
+		}
+		return null;
+	}
+
+	public User updatePerson() {
+		System.out.println("Escolha as opções a serem atualizadas:\n" + "| 1. Atualizar nome\n"
+				+ "| 2. Atualizar data de nascimento\n" + "| 3. Atualizar telefone\n"
+				+ "| 4. Atualizar atualizar CPF\n");
+
+		int opPerson = sc.nextInt();
+		if (opPerson == 1) {
+			Menu.infoCpf();
+			String cpf = sc.next();
+			User person = this.personRepository.findForCpf(cpf);
+			System.out.println("Informe o nome a ser atualizado: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			person.setName(name);
+			person.setDateUpdate(LocalDateTime.now());
+//			student.imprimeDados IMPLEMENTAR
+			System.out.println("| Nome atualizado com sucesso!");
+			return person;
+
+		} else if (opPerson == 2) {
+			Menu.infoCpf();
+			String cpf = sc.next();
+			User person = this.personRepository.findForCpf(cpf);
+			System.out.println("Informe o nome a ser atualizado: ");
+			sc.nextLine();
+			String dtNasc = sc.nextLine();
+			person.setBirthDate(dtNasc);
+			person.setDateUpdate(LocalDateTime.now());
+//			student.imprimeDados IMPLEMENTAR
+			System.out.println("| Data de nascimento atualizada com sucesso!");
+			return person;
+
+		} else if (opPerson == 3) {
+			Menu.infoCpf();
+			String cpf = sc.next();
+			User person = this.personRepository.findForCpf(cpf);
+			System.out.println("Informe o nome a ser atualizado: ");
+			sc.nextLine();
+			String phone = sc.nextLine();
+			person.setPhone(phone);
+			person.setDateUpdate(LocalDateTime.now());
+//			student.imprimeDados IMPLEMENTAR
+			System.out.println("| Telefone atualizado com sucesso!");
+			return person;
+
+		} else if (opPerson == 4) {
+			Menu.infoCpf();
+			String cpf = sc.next();
+			User person = this.personRepository.findForCpf(cpf);
+			System.out.println("Informe o nome a ser atualizado: ");
+			sc.nextLine();
+			String cpfUpdate = sc.nextLine();
+			person.setCpf(cpfUpdate);
+			person.setDateUpdate(LocalDateTime.now());
+//			student.imprimeDados IMPLEMENTAR
+			System.out.println("| CPF atualizado com sucesso!");
+			return person;
+		}
+		return null;
+	}
+
 }
