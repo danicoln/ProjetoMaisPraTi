a9f8f644d747908a952522bd2a1ce84829ec1cfe
danicoln <lincolndeveloper@outlook.com>
2022-10-16 11:16:41 -0300

    Alteração do menu

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -4,11 +4,11 @@ public class Menu {
 
 	public static void menu1() {
 		System.out.println("========= Acesso ao Sistema ========= \n");
-		System.out.println("| 1. Cadastrar pessoa");
-		System.out.println("| 2. Buscar pessoa");
-		System.out.println("| 3. Listar todos alunos");
-		System.out.println("| 4. Remover aluno");
-		System.out.println("| 5. Atualizar aluno");
+		System.out.println("| 1. Cadastrar usuário");
+		System.out.println("| 2. Buscar usuário");
+		System.out.println("| 3. Listar usuários");
+		System.out.println("| 4. Remover usuário");
+		System.out.println("| 5. Atualizar usuário");
 		System.out.println("| 0. Sair do sistema");
 		System.out.println("\n=====================================");
 	}
@@ -17,25 +17,45 @@ public class Menu {
 		System.out.println("Informe o CPF: \n");
 	}
 
-	public static void inserirAluno() {
-		System.out.println("======= Para inserir os dados do aluno =======");
-		System.out.println("======= Digite 1 para inserir os dados =======");
-		System.out.println("======= Digite 2 para voltar ao menu anterior =======");
-	}
-
 	public static void findUser() {
 		System.out.println("======= Escolha a opção desejada =======");
 		System.out.println("| 1. Buscar uma pessoa");
 		System.out.println("| 2. Buscar um aluno");
 		System.out.println("| 3. Encerrar");
 	}
-
-	public static void atualizarAluno() {
-		System.out.println("======= Atualizar aluno =======");		
+	
+	public static void userList() {
+		System.out.println("========= Escolha a opção =========\n");
+		System.out.println("| 1. Listar alunos");
+		System.out.println("| 2. Listar pessoas");
+		System.out.println("| 3. Para voltar ao menu anterior");
 	}
 
-	public static void deletarAluno() {
-		System.out.println("======= Deletar dados do aluno =======");
+	public static void listStudents() {
+		System.out.println("========= Lista de alunos =========\n");
+	}
+	
+	public static void listPerson() {
+		System.out.println("========= Lista de pessoas =========\n");
+	}
+	
+	public static void removeUser() {
+		System.out.println("Escolha a opção desejada: ");
+		System.out.println("| 1. Remover um aluno");
+		System.out.println("| 2. Remover uma pessoa");
+	}
+	
+	public static void confirmRemoval(){
+		System.out.println("Confirma a remoção do aluno?\n");
+		System.out.println("| 1. Confirmar");
+		System.out.println("| 2. Cancelar");
+	}
+
+	public static void updateUser() {
+		System.out.println("========= Digite a opção =========\n");
+		System.out.println("| 1. Atualizar aluno");
+		System.out.println("| 2. Atualizar pessoa");
+		System.out.println("| 3. Retornar ao menu anterior");
 	}
 	
 	
