e5e031baaa16f9767a0f87b788f75298749b168b
danicoln <lincolndeveloper@outlook.com>
2022-10-16 12:10:51 -0300

    Criação da classe ProgramException

---
+++ src/exception/ProgramException.java
@@ -0,0 +1,10 @@
+package exception;
+
+public class ProgramException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public ProgramException(String message) {
+		super(message);
+	}
+}
